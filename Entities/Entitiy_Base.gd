1ae41c83b733f75fd419ae15b473634d2256be5b
Elliot Hipski <elhip03@gmail.com>
2023-05-27 12:42:56 -0400

    made base classes for entity, and will work on scenes

---
+++ Entities/Entitiy_Base.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+class_name Entity_Base
+
+@export var health : int;
+@export var knockback : int;
+@export var friction : int;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
