28b616f9e3cccf7a4dc5fa6c1ff1fc367f46b8ee
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2023-05-28 16:08:29 -0400

    Finish start and quit button

--- User_Interface/Screens/Start_Screen/Start_Screen.gd
+++ User_Interface/Screens/Start_Screen/Start_Screen.gd
@@ -1,4 +1,4 @@
-extends Button
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +11,9 @@ func _process(delta):
 	pass
 
 
-func _on_start_pressed():
+func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://World/world.tscn")
-	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
