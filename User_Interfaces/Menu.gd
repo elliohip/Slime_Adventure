cae826441d3b8742add1d7c5403fc9f00282f753
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2023-05-29 19:00:02 -0400

    Remove focus on the start button

--- User_Interfaces/Menu.gd
+++ User_Interfaces/Menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer/VBoxContainer/ButtonContainer/MarginContainer/StartButton.grab_focus()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
